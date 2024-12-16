4c6c31b2282d7e629d7e7aad8c8f4656f4791d39
nmgbodil <nmgbodille@gmail.com>
2024-12-16 02:47:20 -0600

    Initialized frontend

---
+++ frontend/App.tsx
@@ -0,0 +1,26 @@
+console.log('App.tsx is loaded!');
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to PSEF!</Text>
+      <Text>This is your updated app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#CFB87C',
+  },
+});
